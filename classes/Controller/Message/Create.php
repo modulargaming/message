1c1d7d83ffe55addda99dc534fff33748afc181c
Hinton <hinton.oscar@gmail.com>
2013-07-24 02:11:05 +0200

    Initial commit.

---
+++ classes/Controller/Message/Create.php
@@ -0,0 +1,3 @@
+<?php defined('SYSPATH') OR die('No direct script access.');
+
+class Controller_Message_Create extends MG_Controller_Message_Create {}
---
+++ config/assets.php
@@ -0,0 +1,20 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+
+return array(
+	'frontend' => array(
+		'head' => array(
+			'style' => array(
+				'message' => array(
+					'file' => 'assets/css/message.css'
+				)
+			)
+		),
+		'body' => array(
+			'script' => array(
+				'message' => array(
+					'file' => 'assets/js/message.js'
+				)
+			)
+		)
+	)
+);
